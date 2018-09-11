923eb772ef49e4e2aa5b9fa15ea7f161e1940cfd
alanlamaswolox <alan.lamas@wolox.com.ar>
2018-09-11 16:38:57 -0300

    Added whitespace between devise method and the validates methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :recoverable, :rememberable, :database_authenticatable
+  
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
