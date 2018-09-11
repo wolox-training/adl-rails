d4af60a3918eb457653149323408453dd02303d9
alanlamaswolox <alan.lamas@wolox.com.ar>
2018-09-11 16:37:43 -0300

    Removed encrypted_password's validation in User's model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :recoverable, :rememberable, :database_authenticatable
   validates :email, presence: true
-  validates :encrypted_password, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
