e17776d0cd9538a0de305795190e5df52050a8f4
alanlamaswolox <alan.lamas@wolox.com.ar>
2018-09-12 09:40:03 -0300

    removed whitespace in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :recoverable, :rememberable, :database_authenticatable
-  
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
