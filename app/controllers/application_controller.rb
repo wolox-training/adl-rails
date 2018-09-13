37a0cd6505b0c4cc5e41a5f6e56b9799c2ff36c2
alanlamaswolox <alan.lamas@wolox.com.ar>
2018-09-13 11:14:03 -0300

    gem devise_token_auth added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 # Default controller created by Rails
 class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :exception
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules.
+  include DeviseTokenAuth::Concerns::User
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :recoverable, :rememberable, :database_authenticatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
