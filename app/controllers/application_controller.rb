5431da9465657daef3b14e9c12cc73066a2f02b2
alanlamaswolox <alan.lamas@wolox.com.ar>
2018-09-14 14:20:56 -0300

    whitespace deleted in config/enviroments/development.rb and line deleted in applicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 # Default controller created by Rails
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
   before_action :configure_permitted_parameters, if: :devise_controller?
 
