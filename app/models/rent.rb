243e2bb6c11f293629e191b3e03499ad427afb30
alanlamaswolox <alan.lamas@wolox.com.ar>
2018-09-19 16:09:30 -0300

    Added 'required: true' to Rent model

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,5 +1,5 @@
 class Rent < ApplicationRecord
   validates :user_id, :book_id, :initial_date, :final_date, presence: true
-  belongs_to :user
-  belongs_to :book
+  belongs_to :user, required: true
+  belongs_to :book, required: true
 end
