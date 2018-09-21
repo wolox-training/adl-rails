8f4dadb3c5fc39cfa2396489dd980f1678a7a496
alanlamaswolox <alan.lamas@wolox.com.ar>
2018-09-21 10:59:24 -0300

    deleted files and changed the routes to use the resources way

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-
-    get '/books', to: "books#index"
-    get '/books/:id', to: "books#show"
-    
+    resources :books, only: [:index,:show]
 end
