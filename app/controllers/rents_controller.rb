d4cb48be0f0ca81a797cb48357f0cf2c2cc25db6
adlamas <alan.daniel.lamas@gmail.com>
2018-09-25 16:12:29 -0300

    changed rents_controller

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -3,18 +3,22 @@ class RentsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @rents = Rent.all
+    @rents = Rent.where(user_id: params[:user_id])
     render_paginated @rents
   end
 
   def create
     @rent = Rent.new(rent_params)
-    @rent.save
-  end 
+    if @rent.save
+      render json: { response: 'The rent has been created' }, status: :ok
+    else
+      render json: { errors: @rent.errors.full_messages }, status: :bad_request
+    end
+  end
 
   private
+
   def rent_params
     params.require(:rent).permit(:user_id, :book_id, :initial_date, :final_date)
   end
-
 end
