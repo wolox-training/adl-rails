b821cae335aabc3c44f392be40474109a3ae9e5d
adlamas <alan.daniel.lamas@gmail.com>
2018-09-25 12:25:39 -0300

    changed user's factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
   factory :user do
     email                 { Faker::Internet.email }
-    password              { 'callefalsa123' }
-    password_confirmation { 'callefalsa123' }
+    password              { Faker::Internet.password }
     first_name            { Faker::Name.first_name }
     last_name             { Faker::Name.last_name }
   end
