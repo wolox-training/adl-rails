dbd2516b8c51d347105ae3271d66f7cc7403069c
alanlamaswolox <alan.lamas@wolox.com.ar>
2018-10-03 14:55:25 -0300

    book_suggestion changed

--- db/migrate/20181003125607_create_book_suggestions.rb
+++ db/migrate/20181003125607_create_book_suggestions.rb
@@ -1,14 +1,14 @@
 class CreateBookSuggestions < ActiveRecord::Migration[5.1]
   def change
     create_table :book_suggestions do |t|
-      t.string :synopsis, null: true
-      t.float :price, null: true
+      t.string :synopsis
+      t.float :price
       t.string :author, null: false
       t.string :title, null: false
       t.string :link, null: false
       t.string :publisher, null: false
       t.string :year, null: false
-      t.references :user, index: true, null: true
+      t.references :user, index: true
 
       t.timestamps
     end
