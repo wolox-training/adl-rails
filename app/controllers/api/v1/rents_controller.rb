83a65a1ec60bd779abc37ad2e4dca82cd6036dc7
alanlamaswolox <alan.lamas@wolox.com.ar>
2018-10-03 13:38:54 -0300

    removed rescues from rents_controller

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -4,27 +4,19 @@ module Api
       def index
         @rents = Rent.where(user_id: params[:user_id])
         @user = User.find(params[:user_id])
-        begin
-          authorize(@user)
-          render_paginated @rents
-        rescue Pundit::NotAuthorizedError
-          render json: { errors: "You're not authorized to do this action" }, status: :unauthorized
-        end
+        authorize(@user)
+        render_paginated @rents
       end
 
       def create
         @rent = Rent.new(rent_params)
         @user = User.find(params[:user_id])
-        begin
-          authorize(@user)
-          if @rent.save
-            EmailWorker.perform_async(@rent.id)
-          else
-            render json: { errors: @rent.errors.full_messages }, status: :bad_request
-          end
-        rescue Pundit::NotAuthorizedError
-          render json: { errors: "You're not authorized to do this action" }, status: :unauthorized
-        end
+        authorize(@user)
+        if @rent.save
+          EmailWorker.perform_async(@rent.id)
+        else
+          render json: { errors: @rent.errors.full_messages }, status: :bad_request
+        end      
       end
 
       private
