a36abd87c43695966bc5e93330858786df9b3caa
alanlamaswolox <alan.lamas@wolox.com.ar>
2018-10-03 14:40:44 -0300

    changed rent_controller

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class RentsController < ApiController
+      rescue_from Pundit::NotAuthorizedError, with: :pundit_not_authorized_error
+
       def index
         @rents = Rent.where(user_id: params[:user_id])
         @user = User.find(params[:user_id])
@@ -16,13 +18,17 @@ module Api
           EmailWorker.perform_async(@rent.id)
         else
           render json: { errors: @rent.errors.full_messages }, status: :bad_request
-        end      
+        end
       end
 
       private
 
       def rent_params
         params.require(:rent).permit(:user_id, :book_id, :initial_date, :final_date)
+      end
+
+      def pundit_not_authorized_error
+        render json: { errors: "You're not authorized to do this action" }, status: :unauthorized
       end
     end
   end
