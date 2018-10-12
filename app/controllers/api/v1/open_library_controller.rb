b0016be46b0efd8d023a34a1252c60483a0ff5e1
alanlamaswolox <alan.lamas@wolox.com.ar>
2018-10-12 15:11:57 -0300

    created show view for open_library

--- app/controllers/api/v1/open_library_controller.rb
+++ app/controllers/api/v1/open_library_controller.rb
@@ -3,8 +3,20 @@
 module Api
   module V1
     class OpenLibraryController < ApiController
+        skip_before_action :authenticate_user!, only: [:show] #OJOOO
+
       def show
-        render json: OpenLibraryService.new.isbn_hash(params[:id])
+        respond_to do |format|
+          format.html {render "open_library/show.html.erb"}
+          format.json {render json: OpenLibraryService.new.book_hash(params[:isbn]) }
+        end
+
+        # if peticion.ajax? == false
+            # devolver vista
+        # else
+            # devolver codigo de estado dependiendo de si el servicio encontró o no el libro
+        # end
+
       end
     end
   end
