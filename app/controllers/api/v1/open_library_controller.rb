2ec856e3dcb98dcf3672ecb2b12471c6141f4d38
alanlamaswolox <alan.lamas@wolox.com.ar>
2018-10-16 11:53:12 -0300

    ajax added to get book form

--- app/controllers/api/v1/open_library_controller.rb
+++ app/controllers/api/v1/open_library_controller.rb
@@ -3,20 +3,29 @@
 module Api
   module V1
     class OpenLibraryController < ApiController
-        skip_before_action :authenticate_user!, only: [:show] #OJOOO
+      skip_before_action :authenticate_user!, only: [:show]
 
       def show
         respond_to do |format|
-          format.html {render "open_library/show.html.erb"}
-          format.json {render json: OpenLibraryService.new.book_hash(params[:isbn]) }
+          format.html do
+            render 'open_library/show.html.erb'
+          end
+          format.json do
+            show_json_response
+          end
         end
+      end
 
-        # if peticion.ajax? == false
-            # devolver vista
-        # else
-            # devolver codigo de estado dependiendo de si el servicio encontró o no el libro
-        # end
+      private
 
+      def show_json_response
+        begin
+          json_a_devolver = OpenLibraryService.new.book_hash(params['id'])
+        rescue NoMethodError
+          render json: { errors: "We couldn't find this book, sorry please" }, status: :not_found
+          return
+        end
+        render json: { resp: json_a_devolver }, status: :ok
       end
     end
   end
